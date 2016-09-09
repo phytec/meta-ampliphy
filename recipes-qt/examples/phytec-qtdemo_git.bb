phytec/meta-ampliphy

commit 2b966746a9510991a244c5e08238303dba4e3541

phytec-qt5demo-image: fix qt dependencies
Qtbase depencies are handled correctly now. Remove manual installation
of required packages. Qtbase-fonts is the only one which needs to be
added to phytec-qtdemo RDEPENDS.

Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-qt/examples/phytec-qtdemo_git.bb
@@ -39,7 +39,13 @@ FILES_${PN}-democontent = "${datadir}/${PN}/html ${datadir}/${PN}/images"
 FILES_${PN}-video = "${datadir}/${PN}/videos"
 LICENSE_${PN}-video = "CC-BY-3.0"
 
-RDEPENDS_${PN} += "qtgraphicaleffects-qmlplugins qtmultimedia-qmlplugins qtfreevirtualkeyboard qtquickcontrols-qmlplugins"
+RDEPENDS_${PN} += "\
+    qtgraphicaleffects-qmlplugins \
+    qtmultimedia-qmlplugins \
+    qtfreevirtualkeyboard \
+    qtquickcontrols-qmlplugins \
+    qtbase-fonts \
+"
 RRECOMMENDS_${PN} += "${PN}-democontent ${PN}-videos"
 
 do_install_append() {

recipes-images/images/phytec-qt5demo-image.bb
@@ -20,21 +20,6 @@ IMAGE_INSTALL += "\
     \
     packagegroup-gstreamer \
     \
-    qtbase \
-    qtbase-plugins \
-    qtbase-fonts \
-    qtsvg \
-    qtsvg-plugins \
-    qtdeclarative \
-    qtdeclarative-plugins \
-    qtdeclarative-qmlplugins \
-    qtquickcontrols-qmlplugins \
-    qtmultimedia \
-    qtmultimedia-plugins \
-    qtmultimedia-qmlplugins \
-    qtwebkit \
-    qtwebkit-qmlplugins \
-    qt5everywheredemo \
     qt5-opengles2-test \
     phytec-qtdemo \
 "

commit def641926bb1fbe4814c4f6a897d8e157d352e60

phytec-qtdemo: fix broken webbrowser
In the recipe the dependency to qtwebkit was missing. The last cleanup
commit in the meta-yogurt has revealed that.

Fixes: 2b966746 ("phytec-qt5demo-image: fix qt dependencies")
Signed-off-by: Stefan Lengfeld <s.lengfeld@phytec.de>

recipes-qt/examples/phytec-qtdemo_git.bb
@@ -45,6 +45,7 @@ RDEPENDS_${PN} += "\
     qtfreevirtualkeyboard \
     qtquickcontrols-qmlplugins \
     qtbase-fonts \
+    qtwebkit \
 "
 RRECOMMENDS_${PN} += "${PN}-democontent ${PN}-videos"
 