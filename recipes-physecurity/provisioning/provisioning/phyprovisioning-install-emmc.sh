5cadb3641e4b9d93f716078d4489eb3af41fdd9d
Maik Otto <m.otto@phytec.de>
2022-02-22 13:28:58 +0100

    recipes-physecurity: Add phyprovisoning-emmc
    Add a small script to copy sdcard image to emmc and make init steps for
    the following secure key storage and secure storage initialisation.
    
    Signed-off-by: Maik Otto <m.otto@phytec.de>
    Reviewed-by: Martin Schwan <m.schwan@phytec.de>

---
+++ recipes-physecurity/provisioning/provisioning/phyprovisioning-install-emmc.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+#
+#  provisioning-emmc.sh
+#
+#  Copyright (C) 2022 PHYTEC Messtechnik GmbH,
+#  Author: Maik Otto <m.otto@phytec.de>
+#
+#  Released under the MIT license (see COPYING.MIT for the terms)
+#
+set -e
+trap end EXIT
+end() {
+	if [ "$?" -ne 0 ]; then
+		printf "\n[ERROR] Provisioning eMMC install" 1>&2
+		exit $?
+	fi
+}
+
+version="v0.1"
+SKS_PATH=@SKS_PATH@
+SKS_MOUNTPATH=@SKS_MOUNTPATH@
+SKS_SECRETFOLDER=@SKS_SECRETFOLDER@
+FLASH_PATH=${SKS_PATH%??}
+FILE_SYSTEM=""
+
+usage="
+PHYTEC Install Script ${version} for eMMC
+
+Usage:  $(basename $0) [PARAMETER] [ACTION]
+
+Example:
+    $(basename $0) --filesystem /media/phytec-security-image.rootfs.sdcard --newemmc
+
+One of the following action can be selected:
+    -n | --newemmc    Copy filesystem image to eMMC
+
+The following PARAMETER must be set for eMMC provisioning:
+    -p | --flashpath <flash device> = ${FLASH_PATH}
+    -s | --filesystem <path to sdcard image>
+"
+
+
+#
+# Command line options
+#
+ARGS=$(getopt -n $(basename $0) -o p:s:nvh -l flashpath:,filesystem:,newemmc,version,help -- "$@")
+VALID_ARGS=$?
+if [ "$VALID_ARGS" != "0" ]; then
+	echo "${usage}"
+	exit 2
+fi
+
+eval set -- "$ARGS"
+while :
+do
+	case ${1} in
+	-p | --flashpath) FLASH_PATH="${2}"; shift 2;;
+	-s | --filesystem) FILE_SYSTEM="${2}"; shift 2;;
+	-n | --newemmc)
+		if [ -z "${FLASH_PATH}" ] || [ -z "${FILE_SYSTEM}" ]; then
+			echo "Set flash path and filesystem first!"
+			exit 4
+		fi
+		dd if=${FILE_SYSTEM} of=${FLASH_PATH} bs=100M
+		sync
+		partprobe ${FLASH_PATH}
+		mkdir -p ${SKS_MOUNTPATH}
+		mount ${SKS_PATH} ${SKS_MOUNTPATH}
+		mkdir -p ${SKS_MOUNTPATH}${SKS_SECRETFOLDER}
+		exit 0
+		;;
+	-h | --help)
+		echo "${usage}"
+		exit 0
+		;;
+	-v | --version)
+		echo "${version}"
+		exit 0
+		;;
+	*)
+		echo "Unknown Parameter or Action"
+		echo "${usage}"
+		exit 2
+	esac
+done
