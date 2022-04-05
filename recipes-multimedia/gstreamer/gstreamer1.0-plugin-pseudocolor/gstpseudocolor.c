phytec/meta-ampliphy

commit 7082cf67d51862bdf8dce810a0270dfc1ab95370

layer: convert to new override syntax
This is the result of automated script conversion:
 $ ../poky/scripts/contrib/convert-overrides.py .
converting the metadata to use ":" as the override character instead of "_".
Phytec specific overrides have been added to the script.

Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-multimedia/gstreamer/gstreamer1.0-plugin-pseudocolor/gstpseudocolor.c
@@ -181,12 +181,12 @@ gst_pseudo_color_transform_caps (GstBaseTransform * trans,
 
       if (is_gray) {
         g_value_set_string (&val, "RGB");
-        gst_value_list_append_value (&list, &val);
+        gst_value_list:append_value (&list, &val);
         g_value_unset (&val);
       }
       if (is_rgb) {
         g_value_set_string (&val, "GRAY8");
-        gst_value_list_append_value (&list, &val);
+        gst_value_list:append_value (&list, &val);
         g_value_unset (&val);
       }
 
@@ -197,7 +197,7 @@ gst_pseudo_color_transform_caps (GstBaseTransform * trans,
 
     }
 
-    gst_caps_append_structure (to, structure);
+    gst_caps:append_structure (to, structure);
   }
 
   /* filter the 'to' caps (transform caps) against allowed caps on the pad */

commit 4de4c47623d5787c3674931210784a66dbe3db48

gstreamer1.0-plugin-pseudocolor: Fix accidental function rename
With commit 7082cf67d518 ("layer: convert to new override syntax") some
functions in gstpseudocolor.c have been accidentally changed to the new
override syntax. So fix this.

Fixes: 7082cf67d518 ("layer: convert to new override syntax")

Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-multimedia/gstreamer/gstreamer1.0-plugin-pseudocolor/gstpseudocolor.c
@@ -181,12 +181,12 @@ gst_pseudo_color_transform_caps (GstBaseTransform * trans,
 
       if (is_gray) {
         g_value_set_string (&val, "RGB");
-        gst_value_list:append_value (&list, &val);
+        gst_value_list_append_value (&list, &val);
         g_value_unset (&val);
       }
       if (is_rgb) {
         g_value_set_string (&val, "GRAY8");
-        gst_value_list:append_value (&list, &val);
+        gst_value_list_append_value (&list, &val);
         g_value_unset (&val);
       }
 
@@ -197,7 +197,7 @@ gst_pseudo_color_transform_caps (GstBaseTransform * trans,
 
     }
 
-    gst_caps:append_structure (to, structure);
+    gst_caps_append_structure (to, structure);
   }
 
   /* filter the 'to' caps (transform caps) against allowed caps on the pad */