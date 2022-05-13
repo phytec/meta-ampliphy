phytec/meta-ampliphy

commit da45bd8c3b6383e9fdce1d6843c2443558f8394e

phytec-qtdemo: Fix led control for weston user
The qtdemo is started as weston user when wayland is activated
which causes problems with the user led control.

The led brightness can only be changed as root so weston has no
access. Created a udev rules which grands the needed permissions
to solve this. It is only a workaround until the leds are handled
with gpiolibd in an bigger demo update.

An other problem was the missing PhyKitDemo.conf for the weston user.
The configuration was only copied for root. The qtdemo creates itself
a config file if it is missing but the led numbering did not match
the numbering used in yocto. So the config file is also copied to the
correct place now.

Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
Acked-by: Martin Schwan <m.schwan@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-qt/examples/phytec-qtdemo_git.bb
@@ -9,7 +9,11 @@ LIC_FILES_CHKSUM = " \
     file://${COMMON_LICENSE_DIR}/CC-BY-3.0;md5=dfa02b5755629022e267f10b9c0a2ab7 \
 "
 
-DEPENDS = "qtbase qtdeclarative"
+DEPENDS = " \
+    qtbase \
+    qtdeclarative \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', 'weston-init', '', d)} \
+"
 
 # The resutling packages are machine dependent, because the phytec-qtdemo.service
 # unit is different for ti33x machines.
@@ -21,6 +25,7 @@ SRC_URI = " \
     git://git.phytec.de/phyRDKDemo \
     file://PhyKitDemo.conf \
     file://${SERVICE} \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', 'file://phytec-qtdemo-wl.rules', '', d)} \
 "
 
 SRCREV = "5fd5a82ccee3347be2b9bc61f13ee604b9ae22fd"
@@ -34,7 +39,13 @@ SYSTEMD_SERVICE_${PN} = "phytec-qtdemo.service"
 
 PACKAGES += "${PN}-democontent ${PN}-videos"
 
-FILES_${PN} = "${datadir} ${bindir} ${systemd_unitdir} ${ROOT_HOME}/.config"
+FILES_${PN} = "\
+    ${datadir} \
+    ${bindir} \
+    ${systemd_unitdir} \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', '/home/weston/.config', '${ROOT_HOME}/.config', d)} \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', '${nonarch_base_libdir}', '', d)} \
+"
 FILES_${PN}-dbg = "${datadir}/${BPN}/.debug"
 FILES_${PN}-dev = "/usr/src"
 FILES_${PN}-democontent = "${datadir}/${BPN}/images"
@@ -74,7 +85,14 @@ do_install_append() {
     install -d ${D}${bindir}
     ln -sf ${datadir}/${BPN}/phytec-qtdemo ${D}${bindir}/QtDemo
     install -Dm 0644 ${WORKDIR}/${SERVICE} ${D}${systemd_system_unitdir}/phytec-qtdemo.service
-    install -Dm 0644 ${WORKDIR}/PhyKitDemo.conf ${D}${ROOT_HOME}/.config/Phytec/PhyKitDemo.conf
+
+    if ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', 'true', 'false', d)}; then
+        install -Dm 0644 -o weston -g weston ${WORKDIR}/PhyKitDemo.conf ${D}/home/weston/.config/Phytec/PhyKitDemo.conf
+        install -d ${D}${nonarch_base_libdir}/udev/rules.d/
+        install -m 0644 ${WORKDIR}/phytec-qtdemo-wl.rules ${D}${nonarch_base_libdir}/udev/rules.d/
+    else
+        install -Dm 0644 ${WORKDIR}/PhyKitDemo.conf ${D}${ROOT_HOME}/.config/Phytec/PhyKitDemo.conf
+    fi
 
     # democontent
     install -d ${D}${datadir}/${BPN}/images

recipes-qt/examples/phytec-qtdemo/phytec-qtdemo-wl.rules
@@ -1,0 +1,2 @@
+# allow user-leds brightness control for everyone
+ACTION=="add", SUBSYSTEM=="leds", KERNEL=="user-led[1-3]", RUN+="/bin/chmod a+w /sys%p/brightness"

commit 9230fd531458daa5602430cb11a634521f2367f9

phytec-qtdemo: Fix duplicated FILES setting
Since commit da45bd8c3b63 ("phytec-qtdemo: Fix led control for weston
user") there is a duplicated FILES entry, which effectively cancels out
the intended setting.

Fixes: da45bd8c3b63 ("phytec-qtdemo: Fix led control for weston user")
Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-qt/examples/phytec-qtdemo_git.bb
@@ -46,7 +46,6 @@ FILES:${PN} = "\
     ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', '/home/weston/.config', '${ROOT_HOME}/.config', d)} \
     ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', '${nonarch_base_libdir}', '', d)} \
 "
-FILES:${PN} = "${datadir} ${bindir} ${systemd_unitdir} ${ROOT_HOME}/.config"
 FILES:${PN}-dbg = "${datadir}/${BPN}/.debug"
 FILES:${PN}-dev = "/usr/src"
 FILES:${PN}-democontent = "${datadir}/${BPN}/images"