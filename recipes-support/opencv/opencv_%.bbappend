phytec/meta-ampliphy

commit 17a48997ca6aac0ad20e42bde3b972cc9fb2eaf8

opencv: Build DNN framework
Since opencv 4.5.5, the objdetect module, which is used by the
gstreamer-plugins-bad opencv plugin, now contains a DNN (deep neural
network) based face detection module. Thus in order to build the
objdetect module DNN needs to be enabled.

Otherwise building gstreamer-plugins-bad fails due to the missing
objdetect module.

Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-support/opencv/opencv_%.bbappend
@@ -1,3 +1,4 @@
+PACKAGECONFIG += "dnn"
 EXTRA_OECMAKE:append = " \
     -DINSTALL_BIN_EXAMPLES=ON \
     -DOPENCV_SAMPLES_BIN_INSTALL_PATH=share/opencv4/samples/bin \

commit 8d304d9d846deccb55dfd39ee141ff6afe8c1028

opencv: Fix appending PACKAGECONFIG
Appending a PACKAGECONFIG with a weak default (??=) with += does
not work as expected. The weak default value is not appended but
overridden by the append value.

This comes due to the fact that the weak default value assignment is
made at the end of parsing rather than immediately. The += operator at
the other hand takes immediate effect.

Consequently to be able to append a weak default value the append
overwrite syntax needs to be used.

Fixes: 17a48997ca6a ("opencv: Build DNN framework")
Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-support/opencv/opencv_%.bbappend
@@ -1,4 +1,4 @@
-PACKAGECONFIG += "dnn"
+PACKAGECONFIG:append = " dnn"
 EXTRA_OECMAKE:append = " \
     -DINSTALL_BIN_EXAMPLES=ON \
     -DOPENCV_SAMPLES_BIN_INSTALL_PATH=share/opencv4/samples/bin \