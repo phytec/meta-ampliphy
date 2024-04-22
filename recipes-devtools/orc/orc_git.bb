phytec/meta-ampliphy

commit 9152c8705fe04dce54eb87f5cddda91c483518fd

recipes-devtools: orc: Add recipe for master branch
The current master branch of orc (Optimised Inner Loop Runtime Compiler)
contains support for the aarch64 architecture. Thus add an optional
recipe for i.MX 8 machines to make use of the aarch64 orc support in
gstreamer.

This version is not selected by default but can be selected via
PREFERRED_VERSION.

Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-devtools/orc/orc_git.bb
@@ -1,0 +1,30 @@
+SUMMARY = "Optimised Inner Loop Runtime Compiler"
+HOMEPAGE = "http://gstreamer.freedesktop.org/modules/orc.html"
+LICENSE = "BSD-2-Clause & BSD-3-Clause"
+LIC_FILES_CHKSUM = "file://COPYING;md5=1400bd9d09e8af56b9ec982b3d85797e"
+
+DEFAULT_PREFERENCE = "-1"
+
+SRC_URI = "git://gitlab.freedesktop.org/gstreamer/orc;branch=master"
+SRCREV = "b732715f737595884dd54d1f7860982652b2482f"
+
+PV = "0.4.32+git${SRCPV}"
+
+inherit meson pkgconfig
+
+BBCLASSEXTEND = "native nativesdk"
+
+S = "${WORKDIR}/git"
+
+PACKAGES =+ "orc-examples"
+PACKAGES_DYNAMIC += "^liborc-.*"
+FILES:orc-examples = "${libdir}/orc/*"
+FILES:${PN} = "${bindir}/*"
+
+python populate_packages:prepend () {
+    libdir = d.expand('${libdir}')
+    do_split_packages(d, libdir, r'^lib(.*)\.so\.*', 'lib%s', 'ORC %s library', extra_depends='', allow_links=True)
+}
+
+do_compile:prepend:class-native () {
+    sed -i -e 's#/tmp#.#g' ${S}/orc/orccodemem.c

commit 0d2cac5d4e80d3f19d6b746e5b52e149e55df6b5

recipes-devtools: orc: remove recipe
Commit 6bd8367aa98e ("bitbake: BBHandler: Handle unclosed functions correctly")
now forces a bitbake error upon encountering an unclosed function. This
lead to a build error because the do_compile function in the orc_git
recipe is missing a closing bracket ("}").

As the commit that introduced this error is 2.5 years old and there has
only been one commit for the recipe, it is safe to assume this recipe
has never been tested intially nor has it been tested or even used
since. As a result, remove the recipe as it presents unnecessary
maintenance effort.

Fixes: 9152c8705fe0 ("recipes-devtools: orc: Add recipe for master branch")
Signed-off-by: Yannic Moog <y.moog@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-devtools/orc/orc_git.bb
@@ -1,30 +1,0 @@
-SUMMARY = "Optimised Inner Loop Runtime Compiler"
-HOMEPAGE = "http://gstreamer.freedesktop.org/modules/orc.html"
-LICENSE = "BSD-2-Clause & BSD-3-Clause"
-LIC_FILES_CHKSUM = "file://COPYING;md5=1400bd9d09e8af56b9ec982b3d85797e"
-
-DEFAULT_PREFERENCE = "-1"
-
-SRC_URI = "git://gitlab.freedesktop.org/gstreamer/orc;branch=master"
-SRCREV = "b732715f737595884dd54d1f7860982652b2482f"
-
-PV = "0.4.32+git${SRCPV}"
-
-inherit meson pkgconfig
-
-BBCLASSEXTEND = "native nativesdk"
-
-S = "${WORKDIR}/git"
-
-PACKAGES =+ "orc-examples"
-PACKAGES_DYNAMIC += "^liborc-.*"
-FILES:orc-examples = "${libdir}/orc/*"
-FILES:${PN} = "${bindir}/*"
-
-python populate_packages:prepend () {
-    libdir = d.expand('${libdir}')
-    do_split_packages(d, libdir, r'^lib(.*)\.so\.*', 'lib%s', 'ORC %s library', extra_depends='', allow_links=True)
-}
-
-do_compile:prepend:class-native () {
-    sed -i -e 's#/tmp#.#g' ${S}/orc/orccodemem.c