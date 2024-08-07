phytec/meta-ampliphy

commit a0996945e6b69c75bdb2a90d9c0ab857ed523af6

systemd-conf: Add .network files for AM335+AM57
We now use predictable names for CAN interfaces. This means our
generic can0/can1 .network files don't work anymore. Also provide
dcanX files and add them to the AM335/AM57 BSPs.

Signed-off-by: Daniel Schultz <d.schultz@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-core/systemd/systemd-conf_%.bbappend
@@ -21,6 +21,14 @@ SRC_URI:append:mx8-generic-bsp = " file://45-disable-multitouch-mouse.rules"
 SRC_URI:remove:am62xx = " file://10-watchdog.conf"
 SRC_URI:remove:am62axx = " file://10-watchdog.conf"
 SRC_URI:append:ti-soc =  " file://37-can-ti-soc.rules"
+SRC_URI:append:ti33x = " \
+    file://11-dcan0.conf \
+    file://11-dcan1.conf \
+"
+SRC_URI:append:am57xx = " \
+    file://11-dcan1.conf \
+    file://11-dcan2.conf \
+"
 
 do_install:append() {
     install -d ${D}${systemd_unitdir}/network/

recipes-core/systemd/systemd-conf/11-dcan0.network
@@ -1,0 +1,5 @@
+[Match]
+Name=dcan0
+
+[CAN]
+BitRate=500000

recipes-core/systemd/systemd-conf/11-dcan1.network
@@ -1,0 +1,5 @@
+[Match]
+Name=dcan1
+
+[CAN]
+BitRate=500000

recipes-core/systemd/systemd-conf/11-dcan2.network
@@ -1,0 +1,5 @@
+[Match]
+Name=dcan2
+
+[CAN]
+BitRate=500000

commit 69aba6c7353d3d451ae3036943aac1b7cff02b8d

systemd-conf: Fix SRC_URI for dcan files
Fixes: a099694 ("systemd-conf: Add .network files for AM335+AM57")

SRC_URI appends 11-dcanX.conf files but they actually have the
file extentsion .network.

Signed-off-by: Daniel Schultz <d.schultz@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-core/systemd/systemd-conf_%.bbappend
@@ -22,12 +22,12 @@ SRC_URI:remove:am62xx = " file://10-watchdog.conf"
 SRC_URI:remove:am62axx = " file://10-watchdog.conf"
 SRC_URI:append:ti-soc =  " file://37-can-ti-soc.rules"
 SRC_URI:append:ti33x = " \
-    file://11-dcan0.conf \
-    file://11-dcan1.conf \
+    file://11-dcan0.network \
+    file://11-dcan1.network \
 "
 SRC_URI:append:am57xx = " \
-    file://11-dcan1.conf \
-    file://11-dcan2.conf \
+    file://11-dcan1.network \
+    file://11-dcan2.network \
 "
 
 do_install:append() {