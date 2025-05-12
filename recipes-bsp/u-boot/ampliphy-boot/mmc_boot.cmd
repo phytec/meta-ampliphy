f9ebc752cf57a98f7a3beb6dfe275401abde151b
Wadim Egorov <w.egorov@phytec.de>
2025-05-12 15:00:12 +0200

    recipes-bsp: ampliphy-boot: Support overlays for mmc_boot.cmd
    Add support for loading overlays to mmc_boot.cmd.
    
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>
    Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

--- recipes-bsp/u-boot/ampliphy-boot/mmc_boot.cmd
+++ recipes-bsp/u-boot/ampliphy-boot/mmc_boot.cmd
@@ -1,6 +1,18 @@
 test -n ${mmcroot} || env set mmcroot 2
 test -n ${distro_rootpart} && env set mmcroot ${distro_rootpart}
 env set bootargs "console=${console} earlycon=${earlycon} root=/dev/mmcblk${devnum}p${mmcroot} ${raucargs} rootwait rw ${optargs}"
+env set mmc_load_overlay 'load ${devtype} ${devnum}:${distro_bootpart} ${fdtoverlay_addr_r} ${overlay}'
 load ${devtype} ${devnum}:${distro_bootpart} ${kernel_addr_r} Image
 load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr_r} ${fdtfile}
+
+# Load overlays
+fdt address ${fdt_addr_r}
+for overlay in ${overlays}; do
+  echo Applying overlay: ${overlay};
+  if run mmc_load_overlay; then
+    fdt resize
+    fdt apply ${fdtoverlay_addr_r}
+  fi
+done
+
 booti ${kernel_addr_r} - ${fdt_addr_r}
