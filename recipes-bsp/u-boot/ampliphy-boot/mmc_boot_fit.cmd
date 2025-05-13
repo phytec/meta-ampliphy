12bc49ac3d6ed059da3abb9f9a009c94b41a2447
Wadim Egorov <w.egorov@phytec.de>
2025-05-13 16:04:31 +0200

    recipes-bsp: ampliphy-boot: Support overlays for mmc_boot_fit.cmd
    Add support for loading overlays to mmc_boot_fit.cmd.
    
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>
    Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

--- recipes-bsp/u-boot/ampliphy-boot/mmc_boot_fit.cmd
+++ recipes-bsp/u-boot/ampliphy-boot/mmc_boot_fit.cmd
@@ -2,4 +2,13 @@ test -n ${mmcroot} || env set mmcroot 2
 test -n ${fit_addr_r} && env set loadaddr ${fit_addr_r}
 env set bootargs "console=${console} earlycon=${earlycon} root=/dev/mmcblk${devnum}p${mmcroot} ${raucargs} rootwait rw ${optargs}"
 load ${devtype} ${devnum}:${distro_bootpart} ${loadaddr} fitImage
-bootm ${loadaddr}
+
+# Load overlays
+fdt address ${loadaddr};
+fdt get value fit_default_conf /configurations/ default;
+env set fit_overlay_conf "#${fit_default_conf}";
+for overlay in ${overlays}; do
+  env set fit_overlay_conf "${fit_overlay_conf}#conf-${overlay}";
+done;
+
+bootm ${loadaddr}${fit_overlay_conf}
