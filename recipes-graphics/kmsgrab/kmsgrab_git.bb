phytec/meta-ampliphy

commit 5dfa719fc3c4ea6871e7aa99cd21ed2b0ccbff16

recipes-graphics: Introduce kmsgrab for screenshots on KMS/DRM
Add a recipe for kmsgrab.
kmsgrab is a handy tool for screenshots.

Internally it uses the DRM_IOCTL_MODE_MAP_DUMB ioctl to generate
a dumb of a DRM FB. Most DRM hardware drivers supports Dumb buffers.

Usage:
  kmsgrab <output.png>

Signed-off-by: Wadim Egorov <w.egorov@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-graphics/kmsgrab/kmsgrab_git.bb
@@ -1,0 +1,22 @@
+SUMMARY = "A KMS/DRM screenshot tool"
+HOMEPAGE = "https://github.com/pcercuei/kmsgrab"
+LICENSE = "GPL-2.0"
+LIC_FILES_CHKSUM = "file://${COREBASE}/meta/files/common-licenses/GPL-2.0;md5=801f80980d171dd6425610833a22dbe6"
+
+PV = "1.0"
+PR = "r0"
+
+BRANCH = "main"
+SRC_URI = "git://github.com/pcercuei/kmsgrab.git;protocol=git;branch=${BRANCH}"
+SRCREV = "22916bdc63b133e52079a699ae6b2d25d990e798"
+
+DEPENDS = "libdrm libpng"
+
+S = "${WORKDIR}/git"
+
+inherit cmake
+
+do_install() {
+    install -d ${D}${bindir}
+    install -m 755 ${B}/kmsgrab ${D}${bindir}/kmsgrab
+}

recipes-images/packagegroups/packagegroup-hwtools.bb
@@ -39,6 +39,7 @@ RDEPENDS:${PN} = " \
     ${@bb.utils.contains("MACHINE_FEATURES", "resistivetouch", "tslib-conf tslib-calibrate tslib-tests", "", d)} \
     ${@bb.utils.contains("MACHINE_FEATURES", "pci", "pciutils", "", d)} \
     kmsxx \
+    kmsgrab \
     lmsensors-fancontrol \
     nfs-utils-client \
     dtc \

commit 930ce32b328486b1ceee902f6ea563ef50629dfd

kmsgrab: calculate LIC_FILES_CHKSUM based on source code
The LIC_FILES_CHKSUM variable refers to license of the source code. This
was not the case before (possibly because the source code does not
contain a license file), as a file from the Yocto sources was used as
base.
Use the kmsgrab.c file, as this is the only file in the project
containing any licensing information, as base for the checksum.

[1] https://docs.yoctoproject.org/dev/dev-manual/licenses.html#specifying-the-lic-files-chksum-variable

Fixes: 5dfa719fc3c4 ("recipes-graphics: Introduce kmsgrab for screenshots on KMS/DRM")
Signed-off-by: Yannic Moog <y.moog@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-graphics/kmsgrab/kmsgrab_git.bb
@@ -1,7 +1,7 @@
 SUMMARY = "A KMS/DRM screenshot tool"
 HOMEPAGE = "https://github.com/pcercuei/kmsgrab"
 LICENSE = "GPL-2.0-only"
-LIC_FILES_CHKSUM = "file://${COREBASE}/meta/files/common-licenses/GPL-2.0-only;md5=801f80980d171dd6425610833a22dbe6"
+LIC_FILES_CHKSUM = "file://kmsgrab.c;beginline=1;endline=6;md5=28a1f960814902f2eded43044c600387"
 
 PV = "1.0"
 PR = "r0"