phytec/meta-ampliphy

commit 547c6e636f6f15934378bb107d03778a34e9bb5f

recipes-core: config-partition: Deploy ext4 image
Deploy the config partition as ext4 image to keep permission and
ownership.
Do not use do_install as we do not install the files into the
rootfs.

Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-core/config-partition/config-partition.bb
@@ -7,15 +7,28 @@ inherit deploy
 S = "${WORKDIR}/sources"
 UNPACKDIR = "${S}"
 
-do_install () {
-        install -d ${D}/rauc
-}
+C = "${WORKDIR}/config-partition"
 
-FILES:${PN} = "rauc/"
+fakeroot do_config_partition () {
+        install -d ${C}
+        install -d ${C}/rauc
+
+        tar -czf ${B}/config-partition.tar.gz -C ${C}/ .
+
+        # calculate size of config partition,
+        # needs to be at least 2MB for ext4
+        size=$(du -s ${C} | awk '{print $1}')
+        if [ $size -lt 2048 ]; then size=2048; fi
+        mkfs.ext4 -d ${C} ${B}/config-partition.ext4 $size
+}
+do_config_partition[depends] += " \
+        virtual/fakeroot-native:do_populate_sysroot \
+        e2fsprogs-native:do_populate_sysroot \
+"
+addtask config_partition
 
 do_deploy () {
-        tar -czf ${B}/config-partition.tar.gz -C ${D}/ . --owner=0 --group=0
         install -m 644 ${B}/config-partition.tar.gz ${DEPLOYDIR}
+        install -m 644 ${B}/config-partition.ext4 ${DEPLOYDIR}
 }
-
-addtask deploy after do_install
+addtask deploy after do_config_partition

commit d04bfe08c193c928d53275408734b541047201e7

recipes-core: config-partition: Check ext4 image
Use fsck.ext4 to make sure the created ext4 image is in a good state.

Fixes: 547c6e636f6f ("recipes-core: config-partition: Deploy ext4 image")
Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>
Reviewed-by: Martin Schwan <m.schwan@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-core/config-partition/config-partition.bb
@@ -24,6 +24,7 @@ fakeroot do_config_partition () {
         size=$(du -s ${C} | awk '{print $1}')
         if [ $size -lt 2048 ]; then size=2048; fi
         mkfs.ext4 -d ${C} ${B}/config-partition.ext4 $size
+        fsck.ext4 -pvfD ${B}/config-partition.ext4 || [ $? -le 3 ]
 }
 do_config_partition[depends] += " \
         virtual/fakeroot-native:do_populate_sysroot \