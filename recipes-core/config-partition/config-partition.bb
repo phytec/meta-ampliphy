phytec/meta-ampliphy

commit 547c6e636f6f15934378bb107d03778a34e9bb5f

recipes-core: config-partition: Deploy ext4 image
Deploy the config partition as ext4 image to keep permission and
ownership.
Do not use do_install as we do not install the files into the
rootfs.

Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-core/config-partition/config-partition.bb
@@ -7,15 +7,28 @@ inherit deploy
 S = "${WORKDIR}/sources"
 UNPACKDIR = "${S}"
 
-do_install () {
-        install -d ${D}/rauc
-}
+C = "${WORKDIR}/config-partition"
 
-FILES:${PN} = "rauc/"
+fakeroot do_config_partition () {
+        install -d ${C}
+        install -d ${C}/rauc
+
+        tar -czf ${B}/config-partition.tar.gz -C ${C}/ .
+
+        # calculate size of config partition,
+        # needs to be at least 2MB for ext4
+        size=$(du -s ${C} | awk '{print $1}')
+        if [ $size -lt 2048 ]; then size=2048; fi
+        mkfs.ext4 -d ${C} ${B}/config-partition.ext4 $size
+}
+do_config_partition[depends] += " \
+        virtual/fakeroot-native:do_populate_sysroot \
+        e2fsprogs-native:do_populate_sysroot \
+"
+addtask config_partition
 
 do_deploy () {
-        tar -czf ${B}/config-partition.tar.gz -C ${D}/ . --owner=0 --group=0
         install -m 644 ${B}/config-partition.tar.gz ${DEPLOYDIR}
+        install -m 644 ${B}/config-partition.ext4 ${DEPLOYDIR}
 }
-
-addtask deploy after do_install
+addtask deploy after do_config_partition

commit 01be979d8de2f248bec143a972734563b1b12cde

recipes-core: config-partition: Create build dir
Create the missing build directory ${B} before using it.

Fixes: 547c6e636f6f ("recipes-core: config-partition: Deploy ext4 image")
Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>
Reviewed-by: Martin Schwan <m.schwan@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-core/config-partition/config-partition.bb
@@ -16,6 +16,7 @@ fakeroot do_config_partition () {
                 install -o teesuppl -g teesuppl -m 0700 -d ${C}/optee
         fi
 
+        install -d ${B}
         tar -czf ${B}/config-partition.tar.gz -C ${C}/ .
 
         # calculate size of config partition,