phytec/meta-ampliphy

commit e4a20e06ae56961796bcb1820ab45ba7a7ebcaf3

recipes-securiphy: secure-storage: Add service to open encrypted devices
Add a package physecurestorage-open which contains a systemd service and
a script to open the encrypted root device. Use a custom script because
systemd-cryptsetup does not support trusted and encrypted keys.
A device mapper /dev/mapper/mmcblk1p5 is generated for all partitions on
the eMMC with integrity and/or encryption enabled, e.g.
/dev/mapper/mmcblk0p5 for /dev/mmcblk0p5.

Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>
Reviewed-by: Martin Schwan <m.schwan@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-securiphy/secure-storage/secure-storage/physecurestorage-open.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/sh
+
+root=@FLASH_PATH@p
+count=$(ls ${root}* | wc -l)
+i=1
+while [ $i -le $count ]
+do
+	devroot=${root}${i}
+	for arg in $(blkid ${devroot}); do
+		case "${arg}" in
+			TYPE=*) eval ${arg};;
+			PTTYPE=*) eval ${arg};;
+		esac
+	done
+
+	dm_inc_name=introot${i}
+	if [ -n "${TYPE}" ] && [ "${TYPE}" = "DM_integrity" ]; then
+		integritysetup open ${devroot} --integrity sha256 --journal-integrity sha256 ${dm_inc_name}
+		unset PTTYPE
+		unset TYPE
+		devroot=/dev/mapper/${dm_inc_name}
+		for arg in $(blkid ${devroot}); do
+			case "${arg}" in
+				TYPE=*) eval ${arg};;
+				PTTYPE=*) eval ${arg};;
+			esac
+		done
+	fi
+	dm_final_name=$(basename ${root})${i}
+	if [ ! -n "${TYPE}" ] && [ ! -n "${PTTYPE}" ]; then
+		if test -f ${CONFIG_MOUNTPATH}/secrets/tksecure_key.bb; then
+			dmsetup create ${dm_final_name} --table "0 $(blockdev --getsz ${devroot}) crypt capi:tk(cbc(aes))-plain :64:logon:rootfs: 0 ${devroot} 0"
+		else
+			dmsetup create ${dm_final_name} --table "0 $(blockdev --getsz ${devroot}) crypt aes-xts-plain64 :64:encrypted:rootfs 0 ${devroot} 0"
+		fi
+	fi
+	if [ -e /dev/mapper/${dm_inc_name} ] && [ ! -e /dev/mapper/${dm_final_name} ]; then
+		dmsetup rename ${dm_inc_name} ${dm_final_name}
+	fi
+	let i=i+1
+	unset TYPE
+	unset PTTYPE
+done

recipes-securiphy/secure-storage/physecurestorage_1.6.bb
@@ -3,14 +3,22 @@ LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 FILESEXTRAPATHS:prepend := "${THISDIR}/secure-storage:"
+
+inherit systemd
+
+SYSTEMD_PACKAGES = "${PN}-open"
+SYSTEMD_SERVICE:${PN}-open = "physecurestorage-open.service"
+
 SRC_URI = " \
     file://physecurestorage-install.sh \
+    file://physecurestorage-open.sh \
+    file://physecurestorage-open.service \
 "
 
 S = "${WORKDIR}/sources"
 UNPACKDIR = "${S}"
 
-PACKAGES = "${PN}-install"
+PACKAGES = "${PN}-install ${PN}-open"
 
 FLASH_PATH ??= "/dev/mmcblk${EMMC_DEV}"
 
@@ -18,11 +26,18 @@ do_patch() {
     sed -i \
     -e 's:@FLASH_PATH@:${FLASH_PATH}:g' \
     ${S}/physecurestorage-install.sh
+
+    sed -i \
+    -e 's:@FLASH_PATH@:${FLASH_PATH}:g' \
+    ${S}/physecurestorage-open.sh
 }
 
 do_install() {
     install -d ${D}${bindir}
     install -m 0500 ${S}/physecurestorage-install.sh ${D}${bindir}/physecurestorage-install
+    install -m 0500 ${S}/physecurestorage-open.sh ${D}${bindir}/physecurestorage-open
+    install -d ${D}${systemd_system_unitdir}
+    install -m 0644 ${S}/physecurestorage-open.service ${D}${systemd_system_unitdir}
 }
 
 RDEPENDS:${PN}-install = " \
@@ -35,7 +50,18 @@ RDEPENDS:${PN}-install = " \
     libdevmapper \
     xz \
 "
+RDEPENDS:${PN}-open = " \
+    cryptsetup \
+    libdevmapper \
+    kernel-module-dm-crypt \
+    kernel-module-dm-integrity \
+    util-linux-blkid \
+"
 
 FILES:${PN}-install = "\
     ${bindir}/physecurestorage-install \
+"
+FILES:${PN}-open = "\
+    ${bindir}/physecurestorage-open \
+    ${systemd_system_unitdir}/physecurestorage-open.service \
 "

recipes-securiphy/secure-storage/secure-storage/physecurestorage-open.service
@@ -1,0 +1,16 @@
+[Unit]
+Description=Open encrypted partitions
+DefaultDependencies=no
+After=physecurekeystorage-load.service
+Wants=physecurekeystorage-load.service
+Before=initrd-root-device.target
+
+[Service]
+KeyringMode=shared
+User=root
+ExecStartPre=/usr/bin/keyctl link @u @s
+ExecStart=/usr/bin/physecurestorage-open
+Type=oneshot
+
+[Install]
+WantedBy=initrd.target

commit a59a0c99d54b0e21711e2688d12c1c80f24d0883

recipes-securiphy: secure-storage: physecurestorage-open: Fix logon key
Fix the check for the rootfs encryption key when using secure caam. The
check was broken since CONFIG_MOUNTPATH is never set. Check the name of the
encryption key instead of the key blob path.

Fixes: e4a20e06ae56 ("recipes-securiphy: secure-storage: Add service to open encrypted devices")

Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-securiphy/secure-storage/secure-storage/physecurestorage-open.sh
@@ -28,7 +28,7 @@ do
 	fi
 	dm_final_name=$(basename ${root})${i}
 	if [ ! -n "${TYPE}" ] && [ ! -n "${PTTYPE}" ]; then
-		if test -f ${CONFIG_MOUNTPATH}/secrets/tksecure_key.bb; then
+		if [ $(keyctl list @u | grep "logon: rootfs" | wc -l) -eq 1 ]; then
 			dmsetup create ${dm_final_name} --table "0 $(blockdev --getsz ${devroot}) crypt capi:tk(cbc(aes))-plain :64:logon:rootfs: 0 ${devroot} 0"
 		else
 			dmsetup create ${dm_final_name} --table "0 $(blockdev --getsz ${devroot}) crypt aes-xts-plain64 :64:encrypted:rootfs 0 ${devroot} 0"